1d7f64ed0aa20997dbb299610e1f9698bbd1f880
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2023-09-04 20:33:21 +0900

    added setup script

---
+++ setup.ps1
@@ -0,0 +1,19 @@
+# install PSReadLine(https://github.com/PowerShell/PSReadLine)
+Write-Output "Install PSReadLine"
+Install-Module -Name PowerShellGet -Force
+Install-Module PSReadLine
+
+# install komorebi(https://github.com/LGUG2Z/komorebi)
+Write-Output "Install komorebi"
+winget install LGUG2Z.whkd
+winget install LGUG2Z.komorebi
+Copy-Item User\komorebi.json $Env:USERPROFILE
+Push-Location $Env:USERPROFILE
+komorebic fetch-app-specific-configuration
+Pop-Location
+Copy-Item -Recurse User\.config $Env:USERPROFILE
+
+# install Windows Terminal Preview
+Write-Output "Install WindowsTerminal(Preview Edition)"
+winget install Microsoft.WindowsTerminalPreview
+Copy-Item WIndowsTerminal\settings.json $Env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\LocalState\setings.json
