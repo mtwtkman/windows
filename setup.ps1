b409919b067a3910f038a0521136cc5c8cacb0cc
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2023-10-21 16:17:43 +0900

    added wslconfig

--- setup.ps1
+++ setup.ps1
@@ -9,7 +9,6 @@ Copy-Item WindowsPowerShell -Recurse $Env:USERPROFILE\Documents
 # install GlazeWM(https://github.com/glazerdesktop/GlazeWM)
 Write-Output "Install GlazeWM"
 winget install GlazeWM
-Copy-Item -Recurse User\* $Env:USERPROFILE
 $galzewmExePath = (Get-Command glazewm).Path
 $sh = New-Object -comObject WScript.Shell
 $dest = [System.Environment]::GetFolderPath("StartMenu") + "\galzewm.lnk"
@@ -17,8 +16,10 @@ $shortcut = sh.CreateShortcut($dest)
 $shortcut.TargetPath = $galzewmExePath
 $shortcut.Save()
 
-
 # install Windows Terminal Preview
 Write-Output "Install WindowsTerminal(Preview Edition)"
 winget install Microsoft.WindowsTerminalPreview
 Copy-Item WIndowsTerminal\settings.json $Env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\LocalState\setings.json
+
+Write-Output "Put setting files"
+Copy-Item -Recurse User\* $Env:USERPROFILE
